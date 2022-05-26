5644587f3f4c81b257a99952bcc382f4a9be9115
Simona Bolgradova <simonaivanovab@gmail.com>
2022-05-26 19:46:54 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const handlebars = require('express-handlebars')
 const app = express();
 
 app.use('/static',express.static('public'));
+app.engine('hbs',handlebars.engine({
+    extname:'hbs'
+}))
+
+app.set ('view engine','hbs');
+
+app.set('views','./src/views')
 app.get('/',(req,res)=>{
-    res.send('Hello word');
+   res.render('index')
 });
 
 app.listen(5000,()=>console.log('app is listening on port 5000'))
